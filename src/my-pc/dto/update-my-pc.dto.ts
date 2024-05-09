f36f43c41aa6792f19b75b7fcf4bb00d3094e327
Wilberth Gamboa <50465353+WilberthGamboa@users.noreply.github.com>
2024-05-09 03:12:48 -0600

    Se fixea la subida de archivos y se realizan los test correspondientes

--- src/my-pc/dto/update-my-pc.dto.ts
+++ src/my-pc/dto/update-my-pc.dto.ts
@@ -1,16 +1,23 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateMyPcDto } from './create-my-pc.dto';
-import {  MemoryStoredFile } from 'nestjs-form-data';
+import { HasMimeType, IsFile, MemoryStoredFile } from 'nestjs-form-data';
+import { IsOptional, ValidationArguments } from 'class-validator';
 
 export class UpdateMyPcDto extends PartialType(CreateMyPcDto) {
   id: string;
 
-
+  @IsOptional()
   nombre?: string;
 
-
+  @IsOptional()
   descripcion?: string;
 
-
+  @IsOptional()
+  @HasMimeType(['image/jpeg', 'image/png', 'image/webp'], {
+    message: (validationArguments: ValidationArguments) => {
+      return `La imagen debe tener los siguientes formatos: ${validationArguments.constraints.toString()}`;
+    },
+  })
+  @IsFile({ message: 'La imagen es obligatoria' })
   file?: MemoryStoredFile;
 }
