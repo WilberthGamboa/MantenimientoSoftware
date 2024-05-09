6bcb80ca7d6ea237c62d19f453858afc802a9268
Wilberth Gamboa <50465353+WilberthGamboa@users.noreply.github.com>
2024-05-09 00:46:05 -0600

    init

--- src/auth/helper/errorsFilter.helper.ts
+++ src/auth/helper/errorsFilter.helper.ts
@@ -2,57 +2,12 @@ import { Injectable } from '@nestjs/common';
 import { AuthFormErros } from '../interfaces/AuthFormErros.interfaces';
 @Injectable()
 export class ErrorsFilter {
-  public register(errorMessages: string | string[]): AuthFormErros {
-    const authFormErros: AuthFormErros = {
-      email: [],
-      everyone: [],
-      password: [],
-    };
-
-    if (Array.isArray(errorMessages)) {
-      errorMessages.forEach((errorMessage) => {
-        if (errorMessage.includes('contraseña')) {
-          authFormErros.password.push(errorMessage);
-        } else if (errorMessage.includes('correo')) {
-          authFormErros.email.push(errorMessage);
-        } else {
-          authFormErros.everyone.push(errorMessage);
-        }
-      });
-    } else {
-      if (errorMessages.includes('contraseña')) {
-        authFormErros.password.push(errorMessages);
-      } else if (errorMessages.includes('correo')) {
-        authFormErros.email.push(errorMessages);
-      } else {
-        authFormErros.everyone.push(errorMessages);
-      }
-    }
-
-    return authFormErros;
+  public register(errorMessages: string | string[]): any {
+   
   }
   /* Esta validación se realiza debido a que la implementación de la librería envía automáticamente el valor de Unauthorized 
   esta implemetación envia mensajes personalizados*/
-  public login(errorMessages: string | string[], body: any): AuthFormErros {
-    const authFormErros: AuthFormErros = {
-      email: [],
-      everyone: [],
-      password: [],
-    };
-    if (errorMessages === 'Unauthorized') {
-      errorMessages = '';
-
-      if (body.username === '') {
-        authFormErros.email.push('El correo no puede estar vacio');
-      }
-      if (body.password === '') {
-        authFormErros.password.push('La contraseña no puede estar vacia');
-      }
-    } else {
-      if (!Array.isArray(errorMessages)) {
-        authFormErros.everyone.push(errorMessages);
-      }
-    }
-    return authFormErros;
+  public login(errorMessages: string | string[], body: any): any {
+   
   }
 }
