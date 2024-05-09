550e4df754fea5b40666c8c7c23dcc11cc606f1c
Wilberth Gamboa <50465353+WilberthGamboa@users.noreply.github.com>
2024-05-09 00:58:02 -0600

    init

--- src/auth/helper/errorsFilter.helper.ts
+++ src/auth/helper/errorsFilter.helper.ts
@@ -2,12 +2,25 @@ import { Injectable } from '@nestjs/common';
 import { AuthFormErros } from '../interfaces/AuthFormErros.interfaces';
 @Injectable()
 export class ErrorsFilter {
-  public register(errorMessages: string | string[]): any {
+  public register(errorMessages: string | string[]): AuthFormErros {
+    const authFormErros: AuthFormErros = {
+      email: [],
+      everyone: [],
+      password: [],
+    };
+
    
+    return authFormErros;
   }
   /* Esta validación se realiza debido a que la implementación de la librería envía automáticamente el valor de Unauthorized 
   esta implemetación envia mensajes personalizados*/
-  public login(errorMessages: string | string[], body: any): any {
-   
+  public login(errorMessages: string | string[], body: any): AuthFormErros {
+    const authFormErros: AuthFormErros = {
+      email: [],
+      everyone: [],
+      password: [],
+    };
+  
+    return authFormErros;
   }
 }
